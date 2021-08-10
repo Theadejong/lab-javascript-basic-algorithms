874c76e4da25df307c15e826f0d2472b45dcef1d
Theadejong <theadejong85@gmail.com>
2021-08-10 09:54:39 +0200

    fix error

--- js/index.js
+++ js/index.js
@@ -12,7 +12,7 @@ console.log(`The navigator's name is ${hacker2}`)
         console.log(`The driver has the longest name, it has ${hacker1.length} characters.`)  
         } else if (hacker2.length >= hacker1.length) {    
         console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} characters.`)
-       } else (hacker1.length && hacker2.length) {
+       } else if(hacker1.length && hacker2.length) {
            console.log(`Wow, you both have equally long names, ${hacker2.length} characters!`)
        }   
        
